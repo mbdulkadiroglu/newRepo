d2858119c270d7817fa6b208b33d08240e0adfaf
Mehmet Bahattin Dulkadiroğlu <91752909+mbdulkadiroglu@users.noreply.github.com>
2023-07-26 23:16:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // index.js
 
 
-console.log(addTwoNumbers(2, 3));  // Output: 5
-console.log(subtractTwoNumbers(5, 2));  // Output: 3
+
 
 
 // New method to get the circumference of the circle
@@ -14,6 +13,10 @@ Circle.prototype.getCircumference = function() {
 //some random stuff
 
 // Using the Circle object
+let myCircle = new Circle(5);
+console.log(myCircle.getArea()); // Output: 78.53981633974483
+console.log(myCircle.getCircumference()); // Output: 31.41592653589793
+
 let myCircle = new Circle(5);
 console.log(myCircle.getArea()); // Output: 78.53981633974483
 console.log(myCircle.getCircumference()); // Output: 31.41592653589793
