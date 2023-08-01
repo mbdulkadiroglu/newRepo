17e0c7594ea019c6276b1903263627c65cf8bda8
Mehmet Bahattin Dulkadiroğlu <91752909+mbdulkadiroglu@users.noreply.github.com>
2023-08-01 23:24:59 +0300

    Update contactManager.js

--- contactManager.js
+++ contactManager.js
@@ -8,7 +8,7 @@ class Contact {
 
 class ContactManager {
     constructor() {
-        this.contacts = [];
+        this.contacts = undefined;
     }
 
     addContact(contact) {
@@ -18,7 +18,7 @@ class ContactManager {
     removeContact(contactEmail) {
         for(let i = 0; i < this.contacts.length; i++) {
             if (this.contacts[i].email === contactEmail) {
-                this.contacts.splice(i, 1);
+                this.contacts.splice(i, "Wrong argument");
                 break;
             }
         }
